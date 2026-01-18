e971599aa3de0e65000025ce55f2ab6f478d17aa
Marlene Hense <marlene@MBP-von-Marlene.fritz.box>
2026-01-18 14:48:11 +0100

    adjust naming

--- run_main.sh
+++ run_main.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-echo "----- Job started at $(date)" >> /Users/marlene/CalenderSync/run.log
-cd /Users/marlene/CalenderSync  || exit
-/Users/marlene/CalenderSync/.venv/bin/python main.py
-echo "Script run completed at $(date)" >> /Users/marlene/CalenderSync/run.log
+echo "----- Job started at $(date)" >> /Users/marlene/CalendarSync/run.log
+cd /Users/marlene/CalendarSync  || exit
+/Users/marlene/CalendarSync/.venv/bin/python main.py
+echo "py-script run completed at $(date)" >> /Users/marlene/CalendarSync/run.log
+
+git add calendar.ics
+git commit -m "Update calendar $(date '+%Y-%m-%d %H:%M')"
+git push origin main
+echo "github push completed at $(date)" >> /Users/marlene/CalendarSync/run.log
